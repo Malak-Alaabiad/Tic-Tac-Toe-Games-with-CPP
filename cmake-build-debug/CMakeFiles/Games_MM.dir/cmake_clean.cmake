5c3a7395778da1e9e6e003fc4aed23e4eb450d57
Malak Al-Aabiad <malakkalaabiadd@gmail.com>
2024-12-16 02:19:39 +0200

    Finish

---
+++ cmake-build-debug/CMakeFiles/Games_MM.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Games_MM.dir/main.cpp.obj"
+  "CMakeFiles/Games_MM.dir/main.cpp.obj.d"
+  "Games_MM.exe"
+  "Games_MM.exe.manifest"
+  "Games_MM.pdb"
+  "libGames_MM.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Games_MM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Games_MM.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Games_MM.
+# This may be replaced when dependencies are built.
